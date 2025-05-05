c410bcd4aedac86c37af497d85466498b5f60498
halabalax <gieniopug@gmail.com>
2025-05-06 00:47:15 +0200

    feature (client): add call to auth service
    - add request to auth service from client
    - add axios client based on the request type
    (client or server based)

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,12 +1,22 @@
 import {useState} from 'react';
+import Router from 'next/router'
+import useRequest from '../../hooks/use-request'
 
 export default function Form() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const {doRequest, errors} = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email, password
+        },
+        onSuccess: async () => await Router.push('/')
+    });
 
-    const onSummit = (event) => {
+    const onSummit = async (event) => {
         event.preventDefault();
-        console.log(email, password);
+        await doRequest();
     };
 
     return (
@@ -18,8 +28,10 @@ export default function Form() {
             </div>
             <div className="form-group">
                 <label>Password</label>
-                <input value={password} onChange={e => setPassword(e.target.value)} type="password" className="form-control"/>
+                <input value={password} onChange={e => setPassword(e.target.value)} type="password"
+                       className="form-control"/>
             </div>
+            {errors}
             <button className="btn btn-primary">Sign Up</button>
         </form>
     );
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,18 @@
-export default function Page() {
+import buildClient from '../api/build-client';
+
+const LandingPage = ({currentUser}) => {
+    console.log(currentUser);
     return <h1>Hello Next.js!</h1>
-}
+};
+
+LandingPage.getInitialProps = async (context) => {
+    try {
+        const client = await buildClient(context);
+        const {data} = client.get('/api/users/currentuser');
+        return data;
+    } catch(err) {
+        return err.response.data;
+    }
+};
+
+export default LandingPage;
