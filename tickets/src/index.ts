840bad4e7c5c19db3cebf56ef745e9a9955b5756
hala-bala <gieniopug@gmail.com>
2025-05-21 14:10:58 +0200

    feature (tickets) - move mongo uri to env variable

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -3,10 +3,13 @@ import { app } from './app'
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
-        throw new Error("JWT_KEY is missing!!!")
+        throw new Error("JWT_KEY is missing!!!");
+    }
+    if (!process.env.MONGO_URI) {
+        throw new Error("MONGO_URI is missing!!!");
     }
     try {
-        !await mongoose.connect('mongodb://tickets-mongo-srv:27017/auth');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.log("Error connecting to MongoDB");
